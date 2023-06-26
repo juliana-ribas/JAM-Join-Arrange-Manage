4daacd1d842337cc965727c049c5086b5b1e12ee
jusoffiati <jusoffiati@github.com>
2023-06-26 18:08:13 +0200

    daisyui setup

--- client/src/Components/CreateEventForm.tsx
+++ client/src/Components/CreateEventForm.tsx
@@ -1,14 +1,30 @@
-function CreateEventForm () {
-    return (
-        <>
-        <div>
-            <form>
-                
-                
-            </form>
-        </div>
-        </>
-    )
+declare global {
+  interface Window {
+    my_modal_3: any;
+  }
 }
 
-export default CreateEventForm
+function CreateEventForm() {
+  return (
+    <>
+      <form>
+        <input className="bg-green-500" placeholder="TESTING TAILWIND"></input>
+      </form>
+
+      <button className="btn" onClick={() => window.my_modal_3.showModal()}>
+        open modal
+      </button>
+      <dialog id="my_modal_3" className="modal">
+        <form method="dialog" className="modal-box">
+          <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+            ✕
+          </button>
+          <h3 className="font-bold text-lg">Hello!</h3>
+          <p className="py-4">Press ESC key or click on ✕ button to close</p>
+        </form>
+      </dialog>
+    </>
+  );
+}
+
+export default CreateEventForm;

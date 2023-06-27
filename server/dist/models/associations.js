55d49926e9af2385921999506af11ae29a468ef3
Almun <almun@CW-MBA-Valia.local>
2023-06-27 11:31:14 +0200

    user controller modified

--- server/dist/models/associations.js
+++ server/dist/models/associations.js
@@ -3,24 +3,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.UserEvents = exports.Event = exports.User = void 0;
 const user_1 = __importDefault(require("./user"));
+exports.User = user_1.default;
 const event_1 = __importDefault(require("./event"));
+exports.Event = event_1.default;
 const userEvent_1 = __importDefault(require("./userEvent"));
-user_1.default.hasMany(event_1.default, {
-    foreignKey: 'host',
-});
-user_1.default.belongsToMany(event_1.default, {
-    through: userEvent_1.default,
-    foreignKey: 'userId',
-});
+exports.UserEvents = userEvent_1.default;
 event_1.default.belongsTo(user_1.default, {
     foreignKey: 'host',
+});
+// THIS IS WRONG. USER HAS NO FOREIGN KEY
+user_1.default.hasOne(event_1.default, {
+    foreignKey: 'userId',
 });
 event_1.default.belongsToMany(user_1.default, {
     through: userEvent_1.default,
     foreignKey: 'eventId',
 });
+user_1.default.belongsToMany(event_1.default, {
+    through: userEvent_1.default,
+    foreignKey: 'userId',
+});
 event_1.default.hasMany(userEvent_1.default, {
     foreignKey: 'eventId',
 });
-exports.default = { User: user_1.default, Event: event_1.default, UserEvents: userEvent_1.default };

1b9a10702719958c255453cead115c11bad0ed3b
alepemu <geo.al3x@gmail.com>
2023-06-27 11:31:41 +0200

    fix: models, relations and more

--- server/models/todo.ts
+++ server/models/todo.ts
@@ -1,5 +1,6 @@
 import { DataTypes } from 'sequelize';
 import sequelize from './modelDB';
+import Sequelize from 'sequelize';
 
 const Todo = sequelize.define('Todo', {
   title: {
@@ -10,12 +11,14 @@ const Todo = sequelize.define('Todo', {
     type: DataTypes.BOOLEAN,
     allowNull: false,
   },
-  creatorId: {
-    type: DataTypes.STRING,
+  YcreatorId: {
+    type: Sequelize.UUID,
+    defaultValue: Sequelize.UUIDV4,
     allowNull: false,
   },
-  eventId: {
-    type: DataTypes.STRING,
+  YeventId: {
+    type: Sequelize.UUID,
+    defaultValue: Sequelize.UUIDV4,
     allowNull: false,
   },
 }, { timestamps: false });

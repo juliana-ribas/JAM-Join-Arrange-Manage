da43f458fa648a296009db11b1fca5dd23d7df8d
jusoffiati <jusoffiati@github.com>
2023-06-27 20:52:16 +0200

    EventTiles created

--- client/src/Components/UserDashboard/UserDashboardPage.tsx
+++ client/src/Components/UserDashboard/UserDashboardPage.tsx
@@ -1,9 +1,21 @@
 import CreateEventForm from "./CreateEventForm";
+import EventTile from "./EventTile";
 function UserDashboardPage() {
   return (
     <>
-      <div>TEST - USER DASH</div>
-      <CreateEventForm></CreateEventForm>
+      <div className="flex flex-row">
+        <div className="m-5 w-1/2 h-full flex flex-col gap-5">
+          <EventTile></EventTile>
+          <EventTile></EventTile>
+          <EventTile></EventTile>
+        </div>
+        <div className="m-5 w-1/2 aspect-w-1 aspect-h-1">
+          <div className="mb-5 flex justify-end">
+            <CreateEventForm></CreateEventForm>
+          </div>
+          <div className="bg-green-500 h-64">empty</div>
+        </div>
+      </div>
     </>
   );
 }

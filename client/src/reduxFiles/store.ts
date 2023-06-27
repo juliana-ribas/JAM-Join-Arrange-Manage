b7fb44a85705bda3e22fcb5aa1dcc54ffbc3149b
Codeworks <Codeworks@MacBook-Pro-de-Marc.local>
2023-06-27 20:40:51 +0200

    [FE: CHORE] service added to store

--- client/src/reduxFiles/store.ts
+++ client/src/reduxFiles/store.ts
@@ -3,6 +3,9 @@ import eventReducers from "./slices/events";
 import expenseReducers from "./slices/expenses";
 import toDoReducers from "./slices/toDos";
 import userReducers from "./slices/users";
+import { setupListeners } from '@reduxjs/toolkit/query'
+import { thesisDbApi } from "../services/ThesisDB";
+
 
 const store = configureStore({
   reducer: {
@@ -14,9 +17,16 @@ const store = configureStore({
     toDoReduer: toDoReducers.toDoReducer,
     expenseListReducer: expenseReducers.expenseListReducer,
     expenseReducer: expenseReducers.expenseReducer,
+    [thesisDbApi.reducerPath]: thesisDbApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(thesisDbApi.middleware),
   devTools: true,
 });
+
+// not sure if we need the line below, it is generally optional, but it IS required
+// if we use refetchOnFous or refetchOnReconnect
+// setupListeners(store.dispatch)
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;

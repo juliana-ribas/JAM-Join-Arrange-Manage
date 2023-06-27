a698f51f702cc906c5025878e82c9d30f3b644cd
alepemu <geo.al3x@gmail.com>
2023-06-27 15:59:04 +0200

    [BE:CHORE]: add env variables for running on heroku

--- server/models/modelDB.ts
+++ server/models/modelDB.ts
@@ -1,11 +1,15 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(process.env.DB_NAME || 'Main', process.env.DB_USER || 'postgres', process.env.PW || '2603', {
-  host: 'localhost',
-  dialect: 'postgres',
-  port: 5432,
-  logging: false,
-});
+const { __HEROKU__ } = process.env;
+
+const sequelize = __HEROKU__ 
+  ? new Sequelize(process.env.DB!)
+  : new Sequelize(process.env.DB_NAME || 'Main', process.env.DB_USER || 'postgres', process.env.PW || '2603', {
+    host: 'localhost',
+    dialect: 'postgres',
+    port: 5432,
+    logging: false,
+  });
 
 (async () => {
   try {

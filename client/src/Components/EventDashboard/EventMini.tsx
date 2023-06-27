74cea0457db7275c27b599b8e883e7b3952a3d42
student <student@CW-MBP-Backup.local>
2023-06-27 21:39:45 +0200

    event dashboard basic structure complete

---
+++ client/src/Components/EventDashboard/EventMini.tsx
@@ -0,0 +1,12 @@
+
+
+export default function EventMini () {
+    return (
+        <div className="absolute left-20 top-24 grid grid-cols-2 grid-rows-3">
+           <h3 className="text-2xl col-span-2">Pizza party</h3>
+           <h4 className="text-lg row-span-1 mt-2">🗓️ 29/06/2023</h4>
+           <h4 className="text-lg row-span-1 col-span-1 mt-2">📍 Domino's Pizza</h4>
+           <h4 className="text-lg row-span-2 mt-2">Everyday I'm shufflin' 🕺</h4>
+        </div>
+    )
+}
---
+++ client/src/Components/EventDashboard/Expenses.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Expenses () {
+    return (
+        <div className="flex justify-center">
+        <div className="w-4/5 mx-20 bg-blue-500 rounded-lg flex justify-center mt-56 h-[460px]">
+            <h1 className="p-6 text-2xl text-white">Expenses</h1>
+        </div>
+      </div>
+    )
+}

86916a035bebfd52c830316e151d923b991e8c52
Codeworks <Codeworks@MacBook-Pro-de-Marc.local>
2023-06-28 13:07:02 +0200

    thesisDB.ts get queries updated

--- client/src/services/ThesisDB.ts
+++ client/src/services/ThesisDB.ts
@@ -7,7 +7,8 @@ import { UserState } from '../reduxFiles/slices/users'
 export const thesisDbApi = createApi({
     reducerPath: 'thesisDbApi',
     baseQuery: fetchBaseQuery({ baseUrl: 'https://codeworks-thesis-4063bceaa74a.herokuapp.com/' }),
-    tagTypes: ['EventState', 'ExpenseState', 'ToDoState', 'UserState'],
+    // baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com/' }),
+    // tagTypes: ['EventState', 'ExpenseState', 'ToDoState', 'UserState'],
     endpoints: (build) => ({
         // build.mutation has two type parameters, the first is any, 
         // do we need to fix/address this?
@@ -57,51 +58,58 @@ export const thesisDbApi = createApi({
         //of the parameter we will use to fetch this list, so userID
         getEvents: build.query<EventState[], string>({
             query: (userId) => ({url:`events/${userId}`}),
-            transformResponse: (response: { data: EventState[] }, meta, arg) => response.data,
             //for more complete code and better error handling, refer back to the 
             //and implement the transformErrorResponse, providesTags, OnQueryStarted,
-            //and OnCacheEntryStarted, we may use these for post and patch as well
+            //transformResponse and OnCacheEntryStarted, 
             //https://redux-toolkit.js.org/rtk-query/usage/queries
         }),
         getEvent: build.query<EventState, string>({
-            query: (eventId) => ({url:`events/${eventId}`}),
-            transformResponse: (response: { data: EventState }, meta, arg) => response.data,
+            query: (eventId) => ({url:`event/${eventId}`}),
             //for more complete code and better error handling, refer back to the 
             //and implement the transformErrorResponse, providesTags, OnQueryStarted,
-            //and OnCacheEntryStarted
+            //transformResponse and OnCacheEntryStarted, 
             //https://redux-toolkit.js.org/rtk-query/usage/queries
         }),
         getUsers: build.query<UserState[], string>({
             query: (eventId) => ({url:`users/${eventId}`}),
-            transformResponse: (response: { data: UserState[] }, meta, arg) => response.data,
             //for more complete code and better error handling, refer back to the 
             //and implement the transformErrorResponse, providesTags, OnQueryStarted,
-            //and OnCacheEntryStarted
+            //transformResponse and OnCacheEntryStarted, 
             //https://redux-toolkit.js.org/rtk-query/usage/queries
         }),
-        getUser: build.query<UserState, string>({
-            query: (userId) => ({url:`events/${userId}`}),
-            transformResponse: (response: { data: UserState }, meta, arg) => response.data,
+        getUser: build.query({
+            query: (userId) => ({url:`user/${userId}`}),
             //for more complete code and better error handling, refer back to the 
             //and implement the transformErrorResponse, providesTags, OnQueryStarted,
-            //and OnCacheEntryStarted, 
+            //transformResponse and OnCacheEntryStarted, 
             //https://redux-toolkit.js.org/rtk-query/usage/queries
         }),
         getExpenses: build.query<ExpenseState[], string>({
             query: (eventId) => ({url:`expenses/${eventId}`}),
-            transformResponse: (response: { data: ExpenseState[] }, meta, arg) => response.data,
             //for more complete code and better error handling, refer back to the 
             //and implement the transformErrorResponse, providesTags, OnQueryStarted,
-            //and OnCacheEntryStarted
+            //transformResponse and OnCacheEntryStarted, 
             //https://redux-toolkit.js.org/rtk-query/usage/queries
         }),
         getTodos: build.query<ToDoState[], string>({
             query: (eventId) => ({url:`todos/${eventId}`}),
-            transformResponse: (response: { data: ToDoState[] }, meta, arg) => response.data,
             //for more complete code and better error handling, refer back to the 
             //and implement the transformErrorResponse, providesTags, OnQueryStarted,
-            //and OnCacheEntryStarted
+            //transformResponse and OnCacheEntryStarted, 
             //https://redux-toolkit.js.org/rtk-query/usage/queries
         }),
     }),
   })
+
+  export const {
+    useAddEventMutation,
+    useAddExpenseMutation,
+    useAddToDoMutation,
+    useAddUserMutation,
+    useGetEventQuery,
+    useGetEventsQuery,
+    useGetExpensesQuery,
+    useGetTodosQuery,
+    useGetUserQuery,
+    useGetUsersQuery,
+} = thesisDbApi;

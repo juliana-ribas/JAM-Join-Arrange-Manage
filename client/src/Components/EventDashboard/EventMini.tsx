daf00c5b6d4e91e374d7c1d867ce9bc4b93988d3
student <student@CW-MBP-Backup.local>
2023-06-28 21:34:59 +0200

    feat: rendering todos with delete and done functionalities

--- client/src/Components/EventDashboard/EventMini.tsx
+++ client/src/Components/EventDashboard/EventMini.tsx
@@ -2,7 +2,7 @@
 
 export default function EventMini () {
     return (
-        <div className="absolute left-20 top-24 grid grid-cols-2 grid-rows-3">
+        <div className="absolute left-20 top-24 grid grid-cols-2 grid-rows-3 text-white">
            <h3 className="text-2xl col-span-2">Pizza party</h3>
            <h4 className="text-lg row-span-1 mt-2">🗓️ 29/06/2023</h4>
            <h4 className="text-lg row-span-1 col-span-1 mt-2">📍 Domino's Pizza</h4>

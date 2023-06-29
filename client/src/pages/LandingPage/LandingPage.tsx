bab9e80eefc1cc2cb39ff15f330c94199b3655a0
annakucherenko1 <annakucherenko1997@gmail.com>
2023-06-29 15:23:41 +0200

    feat: landingPage done

--- client/src/pages/LandingPage/LandingPage.tsx
+++ client/src/pages/LandingPage/LandingPage.tsx
@@ -11,7 +11,7 @@ const LandingPage = () => {
   };
 
   return (
-    <section className='landing-page bg-white dark:bg-gray-900' id='hero'>
+    <section className='landing-page bg-white dark:bg-gray-900 lg:mt-9' id='hero'>
       <div className='grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12'>
         <div className='mr-auto place-self-center lg:col-span-7'>
           <h1 className='max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white'>
@@ -22,7 +22,7 @@ const LandingPage = () => {
           </p>
           <button
             onClick={handleGetStartedClick}
-            className='inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-black rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900'
+            className='hidden lg:inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-black rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900'
           >
             Get started
             <svg
@@ -42,13 +42,14 @@ const LandingPage = () => {
         <div className={`lg:col-span-5 ${loginFormActive ? 'active' : ''}`}>
           <div className='login-form  lg:flex lg:flex-col '>
             <LoginForm />
-           <CreateUserForm />
+            <CreateUserForm />
           </div>
         </div>
       </div>
     </section>
   );
 };
+
 
 export default LandingPage;
 

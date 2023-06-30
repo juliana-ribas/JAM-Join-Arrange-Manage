87c8aaeb187138dc28750b5db6a5173003fce471
jusoffiati <jusoffiati@github.com>
2023-06-30 18:07:47 +0200

    delete event button created in temp location

--- client/src/pages/UserDashboardPage.tsx
+++ client/src/pages/UserDashboardPage.tsx
@@ -4,8 +4,12 @@ import { EventState, createEventList } from "../reduxFiles/slices/events";
 import { useSelector } from "react-redux";
 import { RootState, useAppDispatch } from "../reduxFiles/store";
 import { useGetEventsQuery } from "../services/ThesisDB";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { setEventList } from "../reduxFiles/slices/events";
+
+//MOVE TO EVENT PAGE
+import DeleteEvent from "../Components/Event/DeleteEvent";
+//MOVE TO EVENT PAGE
 
 function UserDashboardPage() {
   const dispatch = useAppDispatch();
@@ -26,8 +30,30 @@ function UserDashboardPage() {
   }, [eventList]);
   // add fetch to get all the events
 
+  // THIS NEEDS TO BE MOVED TO THE EVENT PAGE
+  const [deleteModalOpen, setDeleteModalOpen] = useState(false);
+
+  const openDeleteModal = () => {
+    setDeleteModalOpen(true);
+    console.log("event delete button clicked");
+  };
+  // SECTION TO BE MOVED TO EVENT PAGE
+
   return (
     <>
+      {/* THIS NEEDS TO BE MOVED TO THE EVENT PAGE */}
+      <button
+        onClick={() => openDeleteModal()}
+        className="dropdown-item inline-flex w-full justify-center px-3 py-2  text-white focus:outline-none focus:ring-red-300 rounded-md bg-red-600 text-sm font-semibold shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-red-800 sm:ml-3 sm:w-auto"
+      >
+        DELETE
+      </button>
+
+      {deleteModalOpen && (
+        <DeleteEvent setDeleteModalOpen={setDeleteModalOpen} />
+      )}
+      {/* THIS NEEDS TO BE MOVED TO THE EVENT PAGE */}
+
       <div className="flex flex-col justify-center items-center">
         <div className="m-5 h-full p-5 flex flex-col items-center gap-5">
           <CreateEventForm></CreateEventForm>

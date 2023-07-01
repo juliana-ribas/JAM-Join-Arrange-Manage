df5a64892065add5237392fe865fbe8d3456b221
alepemu <geo.al3x@gmail.com>
2023-07-01 12:35:56 +0200

    refactor: event conditional display wip

--- client/src/Components/Event/Event.tsx
+++ client/src/Components/Event/Event.tsx
@@ -1,11 +1,39 @@
+import { useEffect } from "react";
 import EventMini from "../EventDashboard/EventMini";
 import ToggleButton from "../EventDashboard/ToggleButton";
+import { useParams } from 'react-router-dom';
+import { useGetEventQuery } from "../../services/ThesisDB";
+import { useAuth } from "../../utils/useAuth";
+import LoginForm from "../LandingDashboard/LoginForm";
+import CreateUserForm from "../LandingDashboard/CreateUserForm";
 
 export default function Event() {
+
+  const { eventid } = useParams();
+  const { data } = useGetEventQuery(eventid as string)
+  console.log('data', data?.data.title);
+
+  const token = localStorage.getItem('token');
+  console.log(token);
+
+
+
   return (
     <>
-      <EventMini></EventMini>
-      <ToggleButton></ToggleButton>
-    </>
+    <div>
+      {token ? (
+        <>
+          <EventMini></EventMini>
+          <ToggleButton></ToggleButton>
+        </>
+      ) : (
+        <div className="login-form  lg:flex lg:flex-col">
+          <h2>If you want to join an event log in first</h2>
+        <LoginForm />
+        <CreateUserForm />
+      </div>
+      )}
+    </div>
+  </>
   );
 }

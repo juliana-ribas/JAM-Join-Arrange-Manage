03d0a49fdeca2e476a410f0fe899bd72fb6a35cc
annakucherenko1 <annakucherenko1997@gmail.com>
2023-07-03 21:24:59 +0200

    chat working

--- client/src/Components/Chat/ChatContainer.tsx
+++ client/src/Components/Chat/ChatContainer.tsx
@@ -54,68 +54,62 @@ function ChatContainer() {
 
   return (
     <div className="chat-container">
-      
-      <div
-        className="border rounded-lg h-full"
-        style={{ backgroundColor: "#F8F8FF", borderColor: "#D3D3D3" }}
-      >
-        <div className="chat-messages" style={{ width: '100%' }}>
-          <div className="px-5 flex flex-col justify-between">
-            <div className="flex flex-col mt-5">
-              <div className="close" onClick={() => dispatch(closeChat())}>
-                ×
-              </div>
-              <div className="flex justify-end mb-4" ref={messagesRef}>
-                <div className="message-container">
-                  {query.isSuccess &&
-                    data?.map((messageData: any, index) => {
-                      const isCurrentUser = messageData.userId === userId;
-                      // console.log(messageData.User.profilePic)
-                      const messageClassName = `relative text-xs py-2 px-4 shadow rounded-xl m-2 w-80 ${
-                        isCurrentUser ? "bg-blue-400 ml-8" : "bg-gray-300 mr-8"
-                      }`;
-
-                      return (
-                        <div key={messageData.id} className={messageClassName}>
-                          <div className="user">
+    <div className="border rounded-lg h-full" style={{ backgroundColor: "#F8F8FF", borderColor: "#D3D3D3" }}>
+      <div className="chat-messages" style={{ width: '100%' }}>
+        <div className="px-5 flex flex-col justify-between">
+          <div className="flex flex-col mt-5">
+            <div className="close" onClick={() => dispatch(closeChat())}>
+              ×
+            </div>
+            <div className="flex justify-end mb-4" ref={messagesRef}>
+              <div className="message-container">
+                {query.isSuccess &&
+                  data?.map((messageData: any, index) => {
+                    const isCurrentUser = messageData.userId === userId;
+                    const messageClassName = `relative text-xs py-2 px-4 shadow rounded-xl m-2 w-90 ${
+                      isCurrentUser ? "bg-blue-400 ml-8" : "bg-gray-300 mr-8"
+                    }`;
+  
+                    return (
+                      <div key={messageData.id} className={messageClassName}>
+                        <div className="user">
                           <img
                             src={messageData.User.profilePic}
                             className="object-cover h-8 w-8 rounded-full"
                             alt=""
                           />
                           <div className="p-2">{messageData.User.name}</div>
-                          </div>
-                          {/* <div>{messageData.User.profilePic}</div> */}
-                          <div className="message ">{messageData.message}</div>
-                          <div className="time">
-                            {moment(messageData.date).calendar()}{" "}
-                          </div>
                         </div>
-                      );
-                    })}
-                </div>
-                {/* <img
-                  src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
-                  className="object-cover h-8 w-8 rounded-full"
-                  alt=""
-                /> */}
+                        <div className="message">{messageData.message}</div>
+                        <div className="time">
+                          {moment(messageData.date).calendar()}{" "}
+                        </div>
+                      </div>
+                    );
+                  })}
               </div>
             </div>
           </div>
         </div>
-        <div className="input-container">
-          <input
-            className="flex w-full bg-gray-300 py-3 px-3 rounded-xl xl-2 pr-2 mb-0"
-            type="text"
-            placeholder="Type your message here..."
-            value={message}
-            onChange={(event) => setMessage(event.target.value)}
-            onKeyDown={handleKeyDown}
-            name="message"
-          />
-        </div>
+      </div>
+      <div className="input-container">
+        <input
+          className="flex w-full bg-gray-300 py-3 px-3 rounded-xl xl-2 pr-2 mb-0"
+          type="text"
+          placeholder="Type your message here..."
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+          onKeyDown={handleKeyDown}
+          name="message"
+          autoComplete="off" // Disable autocomplete
+          autoCorrect="off" // Disable autocorrect
+          autoCapitalize="off" // Disable autocapitalize
+          spellCheck="false" // Disable spellcheck
+        />
       </div>
     </div>
+  </div>
+  
   );
 }
 

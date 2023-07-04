9fd84516d49a7816c8605478aae3b99cdbc678c6
alepemu <geo.al3x@gmail.com>
2023-07-04 15:17:34 +0200

    refactor: todos page wip

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -23,7 +23,7 @@ function App() {
   return (
     <>
       <div className="App">
-        <Navbar />
+        {/* <Navbar /> */}
         {logoutState ? <Logout /> : null}
         {chatState.isOpen ? <ChatContainer /> : null}
         <Outlet />
--- client/src/Components/EventDashboard/EventData.tsx
+++ client/src/Components/EventDashboard/EventData.tsx
@@ -9,6 +9,8 @@ import { useParams } from "react-router-dom";
 import { PiNotePencilBold } from 'react-icons/pi'
 import { RiDeleteBinLine } from 'react-icons/ri'
 import ToggleButton from "./ToggleButton";
+import { FaCalendarAlt } from 'react-icons/fa'
+import { FaLocationDot } from 'react-icons/fa6'
 
 export default function EventMini({
   eventData,
@@ -30,28 +32,34 @@ export default function EventMini({
     <div>
       {eventData && (
         <>
-          <div className="flex justify-between gap-2 mt-4">
+          <div className="flex h-36 justify-between gap-2 mt-4">
 
-            <div id="event-data-left">
+            <div className="flex w-3/5 gap-3">
 
-              <div className="event-img">
+              <div className="flex shrink-0 bg-slate-400 rounded-xl overflow-hidden">
                 <img src={eventData.data.coverPic} alt="Event picture"></img>
               </div>
 
-              <div className="event-info">
-                <h3 className="text-2xl">
+              <div className="flex flex-col justify-center">
+                <h3 className="text-3xl font-bold">
                   {eventData.data.title}
                 </h3>
 
-                <h4 className="text-lg">
-                  {moment(eventData.data.date).format("ddd, Do MMM - h:mm a")}
-                </h4>
+                <div className="flex items-center">
+                  <FaCalendarAlt className="fill-gray-400"/>
+                  <h4 className="ml-1 text-lg text-gray-400">
+                    {moment(eventData.data.date).format("ddd, Do MMM - h:mm a")}
+                  </h4>
+                </div>
 
-                <h4 className="text-lg">
-                  {eventData.data.location}
-                </h4>
+                <div className="flex items-center">
+                  <FaLocationDot className="fill-gray-400"/>
+                  <h4 className="ml-1 text-lg text-gray-400">
+                    {eventData.data.location}
+                  </h4>
+                </div>
 
-                <h4 className="text-lg">description description description description description description description description description description</h4>
+                <h4 className="mt-1 text-sm leading-tight">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.</h4>
 
 
               </div>
@@ -59,7 +67,7 @@ export default function EventMini({
 
             </div>
 
-            <div id="event-data-right">
+            <div className="flex justify-end w-2/5">
 
               <div className="event-icons">
                 <RiDeleteBinLine size={30} />
@@ -77,7 +85,7 @@ export default function EventMini({
               <h2> SOMETHING </h2>
               )}
               // DELETE BUTTON FROM THE ELSE, ONLY NEEDED SINCE WE DONT HAVE USER TOKEN YET */}
-{/*              
+              {/*              
               <button              
               onClick={handleToggle}
                 className="btn btn-primary"

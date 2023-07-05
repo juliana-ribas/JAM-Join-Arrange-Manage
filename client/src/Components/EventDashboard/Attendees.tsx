03409f6cc6b59721ca860e85f5781cff9166ceb8
alepemu <geo.al3x@gmail.com>
2023-07-05 13:42:09 +0200

    style: attendees no picture fixed

--- client/src/Components/EventDashboard/Attendees.tsx
+++ client/src/Components/EventDashboard/Attendees.tsx
@@ -41,7 +41,7 @@ export default function Attendees() {
       renderedImages.push(
         <div key={i} className="flex flex-col carousel-item m-4">
           <img
-            src={attendees[i].profilePic}
+            src={attendees[i].profilePic? attendees[i].profilePic : "https://res.cloudinary.com/dpzz6vn2w/image/upload/v1688557267/240_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju_wprc8v.jpg"}
             alt={`User${i + 1}`}
             className="rounded-full w-24 h-24 object-cover border-4 border-white"
           />

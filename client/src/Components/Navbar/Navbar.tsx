e8b370372e587f1f602bb6fa547d2811f65169a3
jusoffiati <jusoffiati@github.com>
2023-07-06 11:40:34 +0200

    merge from dev

--- client/src/Components/Navbar/Navbar.tsx
+++ client/src/Components/Navbar/Navbar.tsx
@@ -164,7 +164,11 @@ function Navbar() {
                     </Link>
                   </li>
                   <li>
-                    <button id="signout-btn" onClick={handleSignOut} className="dropdown-item">
+                    <button
+                      id="signout-btn"
+                      onClick={handleSignOut}
+                      className="dropdown-item"
+                    >
                       Sign out
                     </button>
                   </li>

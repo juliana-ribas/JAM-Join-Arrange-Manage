92a678e14e9e9da678ce963b29540dedd48355ec
student <student@CW-MBP-Backup.local>
2023-07-08 19:18:23 +0200

    feat: responsive layout for user dashboard done

--- client/src/pages/UserDashboard/UserDashboardPage.tsx
+++ client/src/pages/UserDashboard/UserDashboardPage.tsx
@@ -1,3 +1,5 @@
+
+
 import CreateEventForm from "../../components/UserDashboard/CreateEventForm";
 import EventTile from "../../components/UserDashboard/EventTile";
 import { EventState } from "../../reduxFiles/slices/events";
@@ -37,7 +39,6 @@ function UserDashboardPage() {
     };
   }, []);
 
-
   useEffect(() => {
     if (!isLoading && !error) {
       dispatch(setEventList(data?.data));
@@ -48,20 +49,17 @@ function UserDashboardPage() {
     .slice()
     .sort((a, b) => moment(a.date).diff(moment(b.date)));
 
-
   const handleToggle = (eventType: string) => {
-    //@ts-ignore
     setShowEvents(eventType);
   };
 
   function AllEvents({ sortedEventList }: { sortedEventList: any }) {
     return (
       <>
-        {" "}
         {sortedEventList.length >= 1 &&
-          sortedEventList.map((event: EventState) => {
-            return <EventTile key={event.eventId} event={event}></EventTile>;
-          })}
+          sortedEventList.map((event: EventState) => (
+            <EventTile key={event.eventId} event={event} />
+          ))}
       </>
     );
   }
@@ -73,7 +71,11 @@ function UserDashboardPage() {
           if (event.UserEvents[0].isHost === true) {
             return <EventTile key={event.eventId} event={event} />;
           }
-          <h3 className="w-full">You are not hosting any events</h3>; // Return null for non-hosted events or handle them differently
+          return (
+            <h3 key={event.eventId} className="w-full">
+              You are not hosting any events
+            </h3>
+          );
         })}
       </>
     );
@@ -85,40 +87,63 @@ function UserDashboardPage() {
         <div className="p-5 flex flex-col justify-start items-center gap-1 align-top w-1/3 h-screen">
           <div className="bg-yellow-30">
             {isLoading && <h2>Loading...</h2>}
-            {!isLoading && sortedEventList.length < 1 && (
+            {!isLoading && sortedEventList.length < 1 ? (
               <div className="flex flex-col justify-center align-middle text-center">
                 <div className="self-center">
-                  <img src="sad-jam.png" className="max-w-[150px]" alt="Sad Jam" />
+                  <img
+                    src="sad-jam.png"
+                    className="max-w-[150px]"
+                    alt="Sad Jam"
+                  />
                 </div>
                 <div>
                   <h3>No upcoming events yet. Click "Host Event" to begin.</h3>
                 </div>
               </div>
-            )}
-            {sortedEventList.length >= 1 && (
-              <div className="w-full flex flex-row align-top justify-center">
-                <button
-                  onClick={() => handleToggle('all')}
-                  className={`btn ${showAllEvents === 'all'
-                    ? 'bg-pink-500 text-white'
-                    : 'bg-pink-100 text-slate-600'
-                    } hover:bg-pink-500 hover:text-white w-1/2 border-0`}
-                >
-                  ALL
-                </button>
-                <button
-                  onClick={() => handleToggle('host')}
-                  className={`btn ${showAllEvents === 'host'
-                    ? 'bg-pink-500 text-white'
-                    : 'bg-pink-100 text-slate-600'
-                    } hover:bg-pink-500 hover:text-white w-1/2 border-0`}
-                >
-                  HOSTING
-                </button>
-              </div>
+            ) : (
+              sortedEventList.length >= 1 && (
+                <div className="w-full flex flex-col align-top justify-center">
+                  {windowWidth < 1100 && (
+                    <div>
+                      <CreateEventForm></CreateEventForm>
+                      <button
+                        className="btn bg-pink-500 text-white hover:bg-pink-700 w-1/2"
+                        onClick={toggleCalendar}
+                      >
+                        {isCalendarVisible ? "Hide Calendar" : "Show Calendar"}
+                      </button>
+                      {isCalendarVisible && (
+                        <div className="popup-calendar absolute z-10 w-1/3 bg-white">
+                          <EventCalendar sortedEventList={sortedEventList} />
+                        </div>
+                      )}
+                    </div>
+                  )}
+                  <div>
+                    <button
+                      onClick={() => handleToggle("all")}
+                      className={`btn ${showAllEvents === "all"
+                        ? "bg-pink-500 text-white"
+                        : "bg-pink-100 text-slate-600"
+                        } hover:bg-pink-500 hover:text-white w-1/2 border-0`}
+                    >
+                      ALL
+                    </button>
+                    <button
+                      onClick={() => handleToggle("host")}
+                      className={`btn ${showAllEvents === "host"
+                        ? "bg-pink-500 text-white"
+                        : "bg-pink-100 text-slate-600"
+                        } hover:bg-pink-500 hover:text-white w-1/2 border-0`}
+                    >
+                      HOSTING
+                    </button>
+                  </div>
+                </div>
+              )
             )}
             <div className="overflow-y-auto">
-              {showAllEvents === 'all' ? (
+              {showAllEvents === "all" ? (
                 <AllEvents sortedEventList={sortedEventList} />
               ) : (
                 <HostedEvents sortedEventList={sortedEventList} />
@@ -127,27 +152,14 @@ function UserDashboardPage() {
           </div>
         </div>
 
-        <div className="p-5 flex flex-col justify-start items-center gap-1 align-top w-1/3 h-full">
-          {windowWidth > 1100 ? (
-            <>
-              <div className="popup-calendar">
-                <EventCalendar sortedEventList={sortedEventList} />
-              </div>
-            </>
-          ) : (
-            <>
-              <button
-                className="btn bg-pink-500 text-white hover:bg-pink-700 w-1/2"
-                onClick={toggleCalendar}
-              >
-                {isCalendarVisible ? 'Hide Calendar' : 'Show Calendar'}
-              </button>
-              {isCalendarVisible && <div className="popup-calendar">
-                <EventCalendar sortedEventList={sortedEventList} />
-              </div>}
-            </>
-          )}
-        </div>
+        {windowWidth > 1100 && (
+          <div className="p-5 flex flex-col justify-start items-center gap-1 align-top w-1/3 h-full">
+            <CreateEventForm></CreateEventForm>
+            <div className="popup-calendar">
+              <EventCalendar sortedEventList={sortedEventList} />
+            </div>
+          </div>
+        )}
       </div>
     </>
   );

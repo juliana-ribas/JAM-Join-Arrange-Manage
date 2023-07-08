86dc8b3ce139586df0b11bb4409631edca01b76e
student <student@CW-MBP-Backup.local>
2023-07-08 17:38:34 +0200

    feat:calendar set to toggle button for smaller screens

--- client/src/pages/UserDashboard/UserDashboardPage.tsx
+++ client/src/pages/UserDashboard/UserDashboardPage.tsx
@@ -18,6 +18,26 @@ function UserDashboardPage() {
 
   const [showAllEvents, setShowEvents] = useState<string>("all");
 
+  const [isCalendarVisible, setCalendarVisible] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const toggleCalendar = () => {
+    setCalendarVisible(!isCalendarVisible);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+
   useEffect(() => {
     if (!isLoading && !error) {
       dispatch(setEventList(data?.data));
@@ -62,14 +82,13 @@ function UserDashboardPage() {
   return (
     <>
       <div className="flex flex-row justify-center align-top top-0 gap-14">
-        <div className=" p-5 flex flex-col justify-start items-center gap-1 align-top w-1/3 h-screen">
-          <div className=" bg-yellow-30">
+        <div className="p-5 flex flex-col justify-start items-center gap-1 align-top w-1/3 h-screen">
+          <div className="bg-yellow-30">
             {isLoading && <h2>Loading...</h2>}
-
             {!isLoading && sortedEventList.length < 1 && (
               <div className="flex flex-col justify-center align-middle text-center">
-                <div className=" self-center">
-                  <img src="sad-jam.png" className="max-w-[150px]" />
+                <div className="self-center">
+                  <img src="sad-jam.png" className="max-w-[150px]" alt="Sad Jam" />
                 </div>
                 <div>
                   <h3>No upcoming events yet. Click "Host Event" to begin.</h3>
@@ -77,32 +96,29 @@ function UserDashboardPage() {
               </div>
             )}
             {sortedEventList.length >= 1 && (
-              <div className="w-full flex flex-row align-top justify-center  ">
+              <div className="w-full flex flex-row align-top justify-center">
                 <button
-                  onClick={() => handleToggle("all")}
-                  className={`btn ${
-                    showAllEvents === "all"
-                      ? "bg-pink-500 text-white"
-                      : "bg-pink-100 text-slate-600"
-                  } hover:bg-pink-500 hover:text-white w-1/2 border-0`}
+                  onClick={() => handleToggle('all')}
+                  className={`btn ${showAllEvents === 'all'
+                    ? 'bg-pink-500 text-white'
+                    : 'bg-pink-100 text-slate-600'
+                    } hover:bg-pink-500 hover:text-white w-1/2 border-0`}
                 >
-                  {/* className="btn flex bg-white items-center gap-2 px-4 ml-4  */}
                   ALL
                 </button>
                 <button
-                  onClick={() => handleToggle("host")}
-                  className={`btn ${
-                    showAllEvents === "host"
-                      ? "bg-pink-500 text-white"
-                      : "bg-pink-100 text-slate-600"
-                  } hover:bg-pink-500 hover:text-white w-1/2 border-0`}
+                  onClick={() => handleToggle('host')}
+                  className={`btn ${showAllEvents === 'host'
+                    ? 'bg-pink-500 text-white'
+                    : 'bg-pink-100 text-slate-600'
+                    } hover:bg-pink-500 hover:text-white w-1/2 border-0`}
                 >
                   HOSTING
                 </button>
               </div>
             )}
             <div className="overflow-y-auto">
-              {showAllEvents === "all" ? (
+              {showAllEvents === 'all' ? (
                 <AllEvents sortedEventList={sortedEventList} />
               ) : (
                 <HostedEvents sortedEventList={sortedEventList} />
@@ -111,11 +127,26 @@ function UserDashboardPage() {
           </div>
         </div>
 
-        <div className=" p-5 flex flex-col justify-start items-center gap-1 align-top w-1/3 h-full">
-          {/* <div className="p-5 flex flex-col "> */}
-          <CreateEventForm></CreateEventForm>
-          <EventCalendar sortedEventList={sortedEventList}></EventCalendar>
-          {/* </div> */}
+        <div className="p-5 flex flex-col justify-start items-center gap-1 align-top w-1/3 h-full">
+          {windowWidth > 1100 ? (
+            <>
+              <div className="popup-calendar">
+                <EventCalendar sortedEventList={sortedEventList} />
+              </div>
+            </>
+          ) : (
+            <>
+              <button
+                className="btn bg-pink-500 text-white hover:bg-pink-700 w-1/2"
+                onClick={toggleCalendar}
+              >
+                {isCalendarVisible ? 'Hide Calendar' : 'Show Calendar'}
+              </button>
+              {isCalendarVisible && <div className="popup-calendar">
+                <EventCalendar sortedEventList={sortedEventList} />
+              </div>}
+            </>
+          )}
         </div>
       </div>
     </>
